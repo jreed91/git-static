46582b6da7eb92d7fa2a0d675ebe5a0dbfc0b822
jreed91 <jreed91@gmail.com>
2015-09-04 14:25:34 -0500

    Initial Commit

---
+++ components/Index.jsx
@@ -0,0 +1,16 @@
+// components/Index.jsx
+var React = require('react')
+var Router = require('react-router')
+var RouteHandler = Router.RouteHandler
+
+var Index = React.createClass({
+  render: function () {
+    return (
+      <main>
+        Index component
+      </main>
+    )
+  }
+})
+
+module.exports = Index
